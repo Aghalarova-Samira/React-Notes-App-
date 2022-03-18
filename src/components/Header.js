57a8cc50db6ea43caf73b3928bca8c8172ce0052
Aghalarova-Samira <aqalarova0@gmail.com>
2022-03-18 18:31:03 +0400

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+
+
+const Header = ({ handleToggleDarkMode }) => {
+
+    return (
+        <div className="header">
+            <h1>Notes</h1>
+            <button
+                onClick={() =>
+                    handleToggleDarkMode(
+                        (previousDarkMode) => !previousDarkMode)}
+                className="save">
+                Toggle Mode
+            </button>
+        </div>
+    )
+}
+
+export default Header;
